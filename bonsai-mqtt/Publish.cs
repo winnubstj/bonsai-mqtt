cfd27b156c761b2787122e61301fa08529caea3b
winnubstj <winnubstj@janelia.hhmi.org>
2020-11-16 22:03:56 -0500

    added solution

---
+++ bonsai-mqtt/Publish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Drawing.Design;
+using System.Collections.Generic;
+
+namespace Bonsai.MQTT
+{
+    [Description("Publish messages on MQTT topic")]
+    public class Publish:Sink<string>
+    {
+        // Settings
+        [Description("IP of MQTT message broker")]
+        public string broker { get; set; } = "127.0.0.1";
+        public Publish()
+        {
+            Console.WriteLine("start up!");
+        }
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            return source.Select(input =>
+            {
+                Console.WriteLine(input);
+                return input;
+
+            });
+        }
+    }
+}
---
+++ bonsai-mqtt/SourceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Drawing.Design;
+using System.Collections.Generic;
+
+namespace Bonsai.MQTT
+{
+    [Description("Receive messages on mqtt topic")]
+    public class SourceTest : Source<int>
+    {
+        public override IObservable<int> Generate()
+        {
+            Console.WriteLine("test");
+            return Observable.Return(0);
+        }
+    }
+}
+
